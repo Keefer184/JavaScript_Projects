c765e936f73a8febf6c55f24522f11ed9d25ffab
Keefer184 <136768491+Keefer184@users.noreply.github.com>
2023-06-22 18:31:57 -0400

    Started JavaScript Course

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,14 @@
+var Y="What up Bitch?"
+window.alert(Y);
+document.write("How it do?");
+var A="This is a string";
+document.write(A);
+var B="Who\'s a hoe? You\'s a hoe.... hoe";
+document.write(B);
+document.write(A+B+"Man bof ya\'ll better shut da fuq up")
+var C="Concatenate"+"String";
+document.write(C);
+alert("I said "+C+" bitch...");
+var x="Mom", d="Dad", m="Mike",j="Jenny";
+document.write(d);
+document.write(3+3);
