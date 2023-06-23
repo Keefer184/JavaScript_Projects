cae8fa3212ecc2cea0451913a7f5cf53cac44fc5
Keefer184 <136768491+Keefer184@users.noreply.github.com>
2023-06-23 16:46:11 -0400

    Added JS effects for Project 2

---
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,11 @@
+function myFirstFunction(){
+    var str="This is button text!";
+    document.getElementById('Button_Text').innerHTML=str;
+    var test="And this is how scripting can chage your document!";
+    document.getElementById("Title").innerHTML=test;
+}
+function myFunction(){
+    var sent="I am learning";
+    sent +=" a lot from this course";
+    document.getElementById("concatenate").innerHTML=sent;
+}
